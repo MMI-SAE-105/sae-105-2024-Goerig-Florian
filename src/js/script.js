a0ca097424ffdfe07191fcc8c68f855ce638bb1d
Goerig-Florian <florian.goerig@edu.univ-fcomte.fr>
2024-12-12 21:32:26 +0100

    carousell

--- src/js/script.js
+++ src/js/script.js
@@ -13,3 +13,23 @@ if (toggle && nav) {
     page.classList.toggle("noscroll", isClosed);
   });
 }
+
+const carousel = document.querySelector(".carousel__container");
+const prevButton = document.querySelector(".carousel__button--prev");
+const nextButton = document.querySelector(".carousel__button--next");
+const premierItem = document.querySelector(".carousel__item");
+
+// Largeur de défilement d’un item
+if (carousel) {
+  // Scroll au clic sur le bouton précédent
+  prevButton.addEventListener("click", () => {
+    const scrollAmount = premierItem.clientWidth;
+    carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+  });
+
+  // Scroll au clic sur le bouton suivant
+  nextButton.addEventListener("click", () => {
+    const scrollAmount = premierItem.clientWidth;
+    carousel.scrollBy({ left: +scrollAmount, behavior: "smooth" });
+  });
+}
